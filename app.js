0d62799fda8e81083212ba2f0f6dd77568864495
Nerea <neresaas@gmail.com>
2024-02-10 00:13:26 +0100

    nuevo

--- app.js
+++ app.js
@@ -25,3 +25,7 @@ app.listen(3000, () => {
 // Para deshacer el git add, utilizaremos el comando ---> git reset
 // Para ver todos los commits dentro del git se utilizará el comando git log
 // Para deshacer el último commit creado se utilizaará el comando ----> git reset --hard HEAD^
+// Para añadir el reservorio local al de Github ----> git remote add origin https://github.com/neresaas/prueba-git.git
+// Luego hay que crear una rama ----> git branch -M main (la rama se llama main). Para consultar las ramas que existen se utilizará el comando ----> git branch
+// Para que el repositorio también desaparezca de GitHub se utilizará el comando ----> git push origin main --force
+// Para traer repositorios que están subidos en Github pero todavía no están en tu repositorio local ----> git pull
